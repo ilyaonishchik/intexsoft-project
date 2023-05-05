b0512d70016d9ab918f573ad89369a437ed8ada0
Ilya Onishchik <ilyaonishchik@gmail.com>
2023-05-05 12:28:37 +0300

    add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './components/home/Home';
+import NotFound from './components/NotFound';
+import Auth from './components/auth/Auth';
+import Catalog from './components/catalog/Catalog';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='catalog' element={<Catalog />} />
+      </Route>
+      <Route path='auth' element={<Auth />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
